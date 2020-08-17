686a7794031b8f437b3ebebc333631c2225d6c57
Ariel Grosh <ariel241202@gmail.com>
2020-08-17 15:45:42 +0300

    added Matrix class

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,154 +1,31 @@
 #include "Matrix.h"
-#include <stdio.h>
-#include <stdlib.h>
 
-extern "C" {
-typedef struct Matrix {
-  double **member;
-  int width;
-  int height;
-} Matrix;
-
-ErrorCode matrix_create(PMatrix *matrix, uint32_t height, uint32_t width) {
-  if (matrix == NULL) {
-    return ERROR_FAILURE_INPUT_ERROR;
-  }
-  *matrix = (Matrix *)malloc(sizeof(Matrix));
-  if (matrix == NULL) {
-    return ERROR_FAILURE_CANT_ALLOCATE;
-  }
-  (*matrix)->width = width;
-  (*matrix)->height = height;
-  (*matrix)->member = (double **)malloc((*matrix)->height * sizeof(double *));
-  if ((*matrix)->member == NULL) {
-    free(*matrix);
-    return ERROR_FAILURE_CANT_ALLOCATE;
-  }
-  for (int i = 0; i < (*matrix)->height; i++) {
-    (*matrix)->member[i] = (double *)calloc((*matrix)->width, sizeof(double));
-    if ((*matrix)->member[i] == NULL) {
-      free((*matrix)->member);
-      free(*matrix);
-      return ERROR_FAILURE_CANT_ALLOCATE;
+class Matrix{
+  public:
+    PMatrix matrix = nullptr;
+    Matrix(int height, int width){
+      matrix_create(&matrix, height, width);
     }
-  }
-  return ERROR_SUCCESS;
-}
-
-ErrorCode matrix_copy(PMatrix *result, CPMatrix source) {
-  if (source == NULL) {
-    return ERROR_FAILURE_INPUT_ERROR;
-  }
-  matrix_create(result, source->height, source->width);
-  if (result == NULL) {
-    return ERROR_FAILURE_CANT_ALLOCATE;
-  }
-  for (int i = 0; i < source->height; i++) {
-    for (int j = 0; j < source->width; j++) {
-      matrix_setValue(*result, i, j, source->member[i][j]);
+    int getHeight(){
+      uint32_t height;
+      matrix_getHeight(matrix, &height);
+      return height;
     }
-  }
-  return ERROR_SUCCESS;
-}
-
-void matrix_destroy(PMatrix matrix) {
-  for (int i = 0; i < matrix->height; i++) {
-    free(matrix->member[i]);
-  }
-  free(matrix->member);
-  free(matrix);
-}
-
-ErrorCode matrix_getHeight(CPMatrix matrix, uint32_t *result) {
-  if (matrix == NULL || matrix->height <= 0) {
-    return ERROR_FAILURE_INPUT_ERROR;
-  }
-  *result = (int)matrix->height;
-  return ERROR_SUCCESS;
-}
-
-ErrorCode matrix_getWidth(CPMatrix matrix, uint32_t *result) {
-  if (matrix == NULL || matrix->height <= 0) {
-    return ERROR_FAILURE_INPUT_ERROR;
-  }
-  *result = (int)matrix->width;
-  return ERROR_SUCCESS;
-}
-
-ErrorCode matrix_setValue(PMatrix matrix, uint32_t rowIndex, uint32_t colIndex,
-                          double value) {
-  if (matrix == NULL || rowIndex > matrix->height || rowIndex < 0 ||
-      colIndex > matrix->width || colIndex < 0) {
-    return ERROR_FAILURE_INPUT_ERROR;
-  }
-  (matrix)->member[rowIndex][colIndex] = value;
-  return ERROR_SUCCESS;
-}
-
-ErrorCode matrix_getValue(CPMatrix matrix, uint32_t rowIndex, uint32_t colIndex,
-                          double *value) {
-  if (matrix == NULL || rowIndex < 0 || rowIndex >= matrix->height ||
-      colIndex < 0 || colIndex >= matrix->width) {
-    return ERROR_FAILURE_INPUT_ERROR;
-  }
-  *value = matrix->member[rowIndex][colIndex];
-  return ERROR_SUCCESS;
-}
-
-ErrorCode matrix_add(PMatrix *result, CPMatrix lhs, CPMatrix rhs) {
-  if ((lhs->height != rhs->height) || (lhs->width != rhs->width) ||
-      lhs == NULL || rhs == NULL) {
-    return ERROR_FAILURE_INPUT_ERROR;
-  }
-  if (result == NULL) {
-    matrix_create(result, lhs->height, lhs->width);
-  }
-  for (int i = 0; i < lhs->height; i++) {
-    for (int j = 0; j < lhs->width; ++j) {
-      matrix_setValue(
-          *result, i, j,
-          lhs->member[i][j] +
-              rhs->member[i][j]); // according to the laws of matrix addition
+    int getWidth(){
+      uint32_t width;
+      matrix_getWidth(matrix, &width);
+      return width;
     }
-  }
-  return ERROR_SUCCESS;
-}
-
-ErrorCode matrix_multiplyMatrices(PMatrix *result, CPMatrix lhs, CPMatrix rhs) {
-  if (lhs == NULL || rhs == NULL ||
-      lhs->width != rhs->height) { // checks if the input is correct
-    return ERROR_FAILURE_INPUT_ERROR;
-  }
-  ErrorCode success = matrix_create(result, lhs->height, rhs->width);
-  if (!error_isSuccess(success)) {
-    return ERROR_FAILURE_CANT_ALLOCATE;
-  }
-  for (int i = 0; i < (*result)->height; i++) {
-    for (int j = 0; j < (*result)->width; j++) {
-      double valueToSet = 0;
-      for (int k = 0; k < lhs->width; k++) {
-        valueToSet +=
-            lhs->member[i][k] *
-            rhs->member[k][j]; // according to the laws of matrix multiplication
-      }
-      matrix_setValue(*result, i, j, valueToSet);
+    void setValue(int i, int j ,double value){
+        matrix_setValue(matrix, i, j, value);
     }
-  }
-  return ERROR_SUCCESS;
-}
-
-ErrorCode matrix_multiplyWithScalar(PMatrix matrix, double scalar) {
-  if (matrix == NULL) {
-    return ERROR_FAILURE_INPUT_ERROR;
-  }
-  for (int i = 0; i < matrix->height; i++) {
-    for (int j = 0; j < matrix->width; j++) {
-      matrix_setValue(
-          matrix, i, j,
-          scalar *
-              matrix->member[i][j]); // according to the laws of linear algebra
+    double getValue(int i, int j){
+      double result;
+      matrix_getValue(matrix, i, j, &result);
+      return result;
     }
-  }
-  return ERROR_SUCCESS;
-}
-}
+    
+    ~Matrix(){
+      matrix_destroy(matrix);
+    }
+};
