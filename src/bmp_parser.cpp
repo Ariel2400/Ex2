6f1dd99b6a0fc7807657c82d19d3185af015ca7b
Ariel Grosh <ariel241202@gmail.com>
2020-08-24 18:24:01 +0300

    deleted magic numbers

--- src/bmp_parser.cpp
+++ src/bmp_parser.cpp
@@ -1,4 +1,12 @@
 #include "bmp_parser.hpp"
+#define BMP_DECLARATION 0x4D42
+#define FILE_INFO_END 14
+#define FILE_DATA_HEAD_END 54
+#define PIXEL_OFFSET 3
+#define BLUE_COEF 0.0722
+#define GREEN_COEF 0.7152
+#define RED_COEF 0.2126
+#define BYTE 4
 
 using std::vector;
 class Matrix;
@@ -16,10 +24,10 @@ void BMP::read(const std::string fname) {
   std::ifstream inp{fname, std::ios_base::binary};
   if (inp) {
     inp.read((char *)&file_header, sizeof(file_header));
-    if (file_header.file_type != 0x4D42) {
+    if (file_header.file_type != BMP_DECLARATION) {
       throw std::runtime_error("Error! Unrecognized file format.");
     }
-    inp.seekg(14, inp.beg);
+    inp.seekg(FILE_INFO_END, inp.beg);
     inp.read((char *)&bmp_info_header, sizeof(bmp_info_header));
 
     // Jump to the pixel data location
@@ -61,13 +69,13 @@ void BMP::write(const std::string fname) {
 void BMP::convert_to_grayscale() {
   for (int i = 0; i < bmp_info_header.height; ++i) {
     for (int j = 0; j < bmp_info_header.width; ++j) {
-      double b = pixels->getValue(i, 3 * j);
-      double g = pixels->getValue(i, 3 * j + 1);
-      double r = pixels->getValue(i, 3 * j + 2);
-      double grayscale = 0.0722 * b + 0.7152 * g + 0.2126 * r;
-      pixels->setValue(i, 3 * j, grayscale);
-      pixels->setValue(i, 3 * j + 1, grayscale);
-      pixels->setValue(i, 3 * j + 2, grayscale);
+      double b = pixels->getValue(i, PIXEL_OFFSET * j);
+      double g = pixels->getValue(i, PIXEL_OFFSET * j + 1);
+      double r = pixels->getValue(i, PIXEL_OFFSET * j + 2);
+      double grayscale = BLUE_COEF * b + GREEN_COEF * g + RED_COEF * r;
+      pixels->setValue(i, PIXEL_OFFSET * j, grayscale);
+      pixels->setValue(i, PIXEL_OFFSET * j + 1, grayscale);
+      pixels->setValue(i, PIXEL_OFFSET * j + 2, grayscale);
     }
   }
   if (bmp_info_header.bit_count == 8) {
@@ -75,7 +83,7 @@ void BMP::convert_to_grayscale() {
       double b = bmp_color_palette.color_list->getValue(i, 0);
       double g = bmp_color_palette.color_list->getValue(i, 1);
       double r = bmp_color_palette.color_list->getValue(i, 2);
-      double grayscale = 0.0722 * b + 0.7152 * g + 0.2126 * r;
+      double grayscale = BLUE_COEF * b + GREEN_COEF * g + RED_COEF * r;
       bmp_color_palette.color_list->setValue(i, 0, grayscale);
       bmp_color_palette.color_list->setValue(i, 1, grayscale);
       bmp_color_palette.color_list->setValue(i, 2, grayscale);
@@ -84,29 +92,29 @@ void BMP::convert_to_grayscale() {
 }
 
 void BMP::rotate_image() {
-  int32_t temp = bmp_info_header.height;
+  uint32_t temp = bmp_info_header.height;
   bmp_info_header.height = bmp_info_header.width;
   bmp_info_header.width = temp;
   Matrix *rotated_pixels =
-      new Matrix(bmp_info_header.height, bmp_info_header.width * 3);
+      new Matrix(bmp_info_header.height, bmp_info_header.width * PIXEL_OFFSET);
   for (int i = 0; i < bmp_info_header.height; ++i) {
     for (int j = 0; j < bmp_info_header.width; ++j) {
       rotated_pixels->setValue(
-          i, 3 * j, pixels->getValue(j, 3 * (bmp_info_header.height - i)));
+          i, PIXEL_OFFSET * j, pixels->getValue(j, PIXEL_OFFSET * (bmp_info_header.height - i)));
       rotated_pixels->setValue(
-          i, 3 * j + 1,
-          pixels->getValue(j, 3 * (bmp_info_header.height - i) + 1));
+          i, PIXEL_OFFSET * j + 1,
+          pixels->getValue(j, PIXEL_OFFSET * (bmp_info_header.height - i) + 1));
       rotated_pixels->setValue(
-          i, 3 * j + 2,
-          pixels->getValue(j, 3 * (bmp_info_header.height - i) + 2));
+          i, PIXEL_OFFSET * j + 2,
+          pixels->getValue(j, PIXEL_OFFSET * (bmp_info_header.height - i) + 2));
     }
   }
-  pixels = new Matrix(bmp_info_header.height, bmp_info_header.width * 3);
+  pixels = new Matrix(bmp_info_header.height, bmp_info_header.width * PIXEL_OFFSET);
   for (int i = 0; i < bmp_info_header.height; ++i) {
     for (int j = 0; j < bmp_info_header.width; ++j) {
-      pixels->setValue(i, 3 * j, rotated_pixels->getValue(i, 3 * j));
-      pixels->setValue(i, 3 * j + 1, rotated_pixels->getValue(i, 3 * j + 1));
-      pixels->setValue(i, 3 * j + 2, rotated_pixels->getValue(i, 3 * j + 2));
+      pixels->setValue(i, PIXEL_OFFSET * j, rotated_pixels->getValue(i, PIXEL_OFFSET * j));
+      pixels->setValue(i, PIXEL_OFFSET * j + 1, rotated_pixels->getValue(i, PIXEL_OFFSET * j + 1));
+      pixels->setValue(i, PIXEL_OFFSET * j + 2, rotated_pixels->getValue(i, PIXEL_OFFSET * j + 2));
     }
   }
   if (bmp_info_header.bit_count == 8) {
@@ -180,10 +188,10 @@ uint32_t BMP::make_stride_aligned(uint32_t align_stride) {
 }
 
 void BMP::write_24_bit(std::ofstream &of, vector<uint8_t> data) {
-  if (bmp_info_header.width % 4 == 0) {
+  if (bmp_info_header.width % BYTE == 0) {
     write_headers_and_data(of, data);
   } else {
-    uint32_t new_stride = make_stride_aligned(4);
+    uint32_t new_stride = make_stride_aligned(BYTE);
     std::vector<uint8_t> padding_row(new_stride - row_stride);
 
     write_headers(of);
@@ -202,17 +210,17 @@ void BMP::write_8_bit(std::ofstream &of) {
   std::vector<uint8_t> color_palette_padding(1);
   int num_of_colors = bmp_color_palette.color_list->getHeight();
   for (int i = 0; i < num_of_colors; ++i) {
-    of.write((const char *)(color_palette.data() + 3 * i), 3);
+    of.write((const char *)(color_palette.data() + PIXEL_OFFSET * i), PIXEL_OFFSET);
     of.write((const char *)color_palette_padding.data(), 1);
   }
   std::vector<uint8_t> pixel_color_indexes =
       matrix_to_vector(bmp_color_palette.pixel_color_indexes);
-  if (bmp_info_header.width % 4 == 0) {
+  if (bmp_info_header.width % BYTE == 0) {
     of.write((const char *)pixel_color_indexes.data(),
              pixel_color_indexes.size());
   } else {
     // uint32_t image_stride = bmp_info_header.width;
-    uint32_t new_stride = make_stride_aligned(4);
+    uint32_t new_stride = make_stride_aligned(BYTE);
     std::vector<uint8_t> pixel_color_indexes_padding(new_stride - row_stride);
     for (int i = 0; i < bmp_info_header.height; ++i) {
       of.write((const char *)(pixel_color_indexes.data() + row_stride * i),
@@ -224,13 +232,13 @@ void BMP::write_8_bit(std::ofstream &of) {
 }
 
 void BMP::read_24_bit(std::ifstream *inp, vector<uint8_t> data) {
-  if (bmp_info_header.width % 4 == 0) {
+  if (bmp_info_header.width % BYTE == 0) {
     inp->read((char *)data.data(), data.size());
     file_header.file_size += data.size();
   } else {
     uint32_t row_stride_24bit =
         bmp_info_header.width * bmp_info_header.bit_count / 8;
-    uint32_t new_stride = make_stride_aligned(4);
+    uint32_t new_stride = make_stride_aligned(BYTE);
     std::vector<uint8_t> padding_row(new_stride - row_stride);
     for (int i = 0; i < bmp_info_header.height; ++i) {
       inp->read((char *)(data.data() + row_stride_24bit * i), row_stride_24bit);
@@ -240,27 +248,27 @@ void BMP::read_24_bit(std::ifstream *inp, vector<uint8_t> data) {
         data.size() + bmp_info_header.height * padding_row.size();
   }
   pixels =
-      vector_to_matrix(data, bmp_info_header.height, bmp_info_header.width * 3);
+      vector_to_matrix(data, bmp_info_header.height, bmp_info_header.width * PIXEL_OFFSET);
 }
 
 void BMP::read_8_bit(std::ifstream *inp) {
-  inp->seekg(54, inp->beg);
-  int color_palette_size = file_header.offset_data - 54;
-  int num_of_colors = color_palette_size / 4;
-  std::vector<uint8_t> bgr_values(num_of_colors * 3);
+  inp->seekg(FILE_DATA_HEAD_END, inp->beg);
+  int color_palette_size = file_header.offset_data - FILE_DATA_HEAD_END;
+  int num_of_colors = color_palette_size / BYTE;
+  std::vector<uint8_t> bgr_values(num_of_colors * PIXEL_OFFSET);
   std::vector<uint8_t> padding(1);
   for (int i = 0; i < num_of_colors; ++i) {
-    inp->read((char *)(bgr_values.data() + 3 * i), 3);
+    inp->read((char *)(bgr_values.data() + PIXEL_OFFSET * i), PIXEL_OFFSET);
     inp->read((char *)padding.data(), 1);
   }
-  bmp_color_palette.color_list = vector_to_matrix(bgr_values, num_of_colors, 3);
+  bmp_color_palette.color_list = vector_to_matrix(bgr_values, num_of_colors, PIXEL_OFFSET);
   std::vector<uint8_t> color_indexes(bmp_info_header.height *
                                      bmp_info_header.width);
-  if (bmp_info_header.width % 4 == 0) {
+  if (bmp_info_header.width % BYTE == 0) {
     inp->read((char *)color_indexes.data(), color_indexes.size());
   } else {
     uint32_t row_stride_8bit = bmp_info_header.width;
-    uint32_t new_stride = make_stride_aligned(4);
+    uint32_t new_stride = make_stride_aligned(BYTE);
     std::vector<uint8_t> padding_row(new_stride - row_stride);
     for (int i = 0; i < bmp_info_header.height; ++i) {
       inp->read((char *)(color_indexes.data() + row_stride_8bit * i),
@@ -270,19 +278,19 @@ void BMP::read_8_bit(std::ifstream *inp) {
   }
   bmp_color_palette.pixel_color_indexes = vector_to_matrix(
       color_indexes, bmp_info_header.height, bmp_info_header.width);
-  pixels = new Matrix(bmp_info_header.height, bmp_info_header.width * 3);
+  pixels = new Matrix(bmp_info_header.height, bmp_info_header.width * PIXEL_OFFSET);
   for (int i = 0; i < bmp_info_header.height; ++i) {
     for (int j = 0; j < bmp_info_header.width; ++j) {
       pixels->setValue(
-          i, 3 * j,
+          i, PIXEL_OFFSET * j,
           bmp_color_palette.color_list->getValue(
               bmp_color_palette.pixel_color_indexes->getValue(i, j), 0));
       pixels->setValue(
-          i, 3 * j + 1,
+          i, PIXEL_OFFSET * j + 1,
           bmp_color_palette.color_list->getValue(
               bmp_color_palette.pixel_color_indexes->getValue(i, j), 1));
       pixels->setValue(
-          i, 3 * j + 2,
+          i, PIXEL_OFFSET * j + 2,
           bmp_color_palette.color_list->getValue(
               bmp_color_palette.pixel_color_indexes->getValue(i, j), 2));
     }
